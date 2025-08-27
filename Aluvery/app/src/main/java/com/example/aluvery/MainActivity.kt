e7e65d8cf317b0c81d201f15eb662ba22a96a0e7
Josue Carlos <josuecarlosos2@gmail.com>
2025-08-27 10:55:19 -0300

    compreendendo a preview

--- Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
+++ Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -4,13 +4,18 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.aluvery.ui.theme.AluveryTheme
 
 class MainActivity : ComponentActivity() {
@@ -19,29 +24,60 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             AluveryTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                Surface {
+                    MyFirstComposable()
                 }
             }
         }
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyFirstComposable(){
+    Column(
+        Modifier
+            .padding(30.dp, 20.dp, 10.dp, 0.dp)
+            .background(color = Color.Blue)
+            .fillMaxWidth()
+    ) {
+        Column {
+            Text("olá mundo")
+            Text("olá mundo")
+        }
+
+        Row (
+            Modifier.background(color = Color.Red)
+        ) {
+            Text("olá mundo")
+            Text("olá mundo")
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
+@Preview(
+    name = "Initial",
+    showSystemUi = true,
+)
 @Composable
-fun GreetingPreview() {
+fun Prev (){
     AluveryTheme {
-        Greeting("Android")
+        Surface {
+            MyFirstComposable()
+        }
+    }
+}
+
+@Preview(
+    name = "Initiall",
+    showSystemUi = true,
+)
+@Composable
+fun Prev2 (){
+    AluveryTheme {
+        Surface {
+            MyFirstComposable()
+        }
     }
 }
