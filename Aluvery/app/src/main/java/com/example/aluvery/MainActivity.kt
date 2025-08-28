b6206bb56e00ae6dd3ec2b9b4765312ec43a96ce
Josue Carlos <josuecarlosos2@gmail.com>
2025-08-28 18:54:56 -0300

    block

--- Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
+++ Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -4,16 +4,21 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.example.aluvery.ui.theme.AluveryTheme
@@ -25,7 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             AluveryTheme {
                 Surface {
-                    MyFirstComposable()
+                    ProductItem()
                 }
             }
         }
@@ -33,51 +38,24 @@ class MainActivity : ComponentActivity() {
 }
 
 
+@Preview
 @Composable
-fun MyFirstComposable(){
-    Column(
-        Modifier
-            .padding(30.dp, 20.dp, 10.dp, 0.dp)
-            .background(color = Color.Blue)
-            .fillMaxWidth()
-    ) {
-        Column {
-            Text("olá mundo")
-            Text("olá mundo")
-        }
+private fun ProductItem() {
+  Column { 
+      Box (
+          Modifier
+              .height(50.dp)
+              .background(Color.Blue)
+              .width(50.dp)
+      )
 
-        Row (
-            Modifier.background(color = Color.Red)
-        ) {
-            Text("olá mundo")
-            Text("olá mundo")
-        }
-    }
-}
+      Image(
+          painter = painterResource(id = R.drawable.ic_launcher_background),
+          contentDescription = "iamge"
+      )
 
+      Text(text = "Tex 1")
+      Text(text = "Tex 2")
 
-@Preview(
-    name = "Initial",
-    showSystemUi = true,
-)
-@Composable
-fun Prev (){
-    AluveryTheme {
-        Surface {
-            MyFirstComposable()
-        }
-    }
-}
-
-@Preview(
-    name = "Initiall",
-    showSystemUi = true,
-)
-@Composable
-fun Prev2 (){
-    AluveryTheme {
-        Surface {
-            MyFirstComposable()
-        }
-    }
+  }
 }
