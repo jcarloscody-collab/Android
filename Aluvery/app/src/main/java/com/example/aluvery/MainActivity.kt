a99a186b84f4d4200a33d16c11c0debbba219ace
Josue Carlos <josuecarlosos2@gmail.com>
2025-09-05 00:42:53 -0300

    criacao do componente de box

--- Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
+++ Aluvery/app/src/main/java/com/example/aluvery/MainActivity.kt
@@ -9,19 +9,36 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.heightIn
+import androidx.compose.foundation.layout.offset
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.datasource.LoremIpsum
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.aluvery.ui.theme.AluveryTheme
+import com.example.aluvery.ui.theme.Purple40
+import com.example.aluvery.ui.theme.Purple500
+import com.example.aluvery.ui.theme.Teal200
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -41,21 +58,61 @@ class MainActivity : ComponentActivity() {
 @Preview
 @Composable
 private fun ProductItem() {
-  Column { 
-      Box (
-          Modifier
-              .height(50.dp)
-              .background(Color.Blue)
-              .width(50.dp)
-      )
+    Surface (
+        Modifier.padding(20.dp),
+        shape = RoundedCornerShape(15.dp),
+        shadowElevation = 10.dp
+    ) {
+        val imageSize = 100.dp
+        Column(
+            Modifier
+                .heightIn((2*imageSize.value + imageSize.value/2).dp)
+                .width((2*imageSize.value).dp)
+        ) {
+            Box(
+                Modifier
+                    .height(
+                        imageSize
+                    )
+                    .background(
+                        brush = Brush.horizontalGradient(
+                            colors = listOf(
+                                Purple500, Teal200
+                            )
+                        )
+                    )
+                    .fillMaxWidth()
+            ) {
+                Image(
+                    painter = painterResource(id = R.drawable.ic_launcher_background),
+                    contentDescription = "iamge",
+                    Modifier
+                        .size(imageSize)
+                        .offset(y = imageSize/2)
+                        .clip(shape = CircleShape)
+                        .align(Alignment.BottomCenter)
 
-      Image(
-          painter = painterResource(id = R.drawable.ic_launcher_background),
-          contentDescription = "iamge"
-      )
+                )
+            }
 
-      Text(text = "Tex 1")
-      Text(text = "Tex 2")
+            Spacer(Modifier.height(imageSize/2))
+            Column(Modifier.padding(16.dp)) {
+                Text(
+                    text = LoremIpsum(50).values.first(),
+                    fontSize = 18.sp,
+                    fontWeight = FontWeight(700),
+                    maxLines = 2,
+                    overflow = TextOverflow.Ellipsis,
 
-  }
+                    )
+                Text(
+                    text = "R$ 14.00", Modifier.padding(top = 8.dp),
+                    fontSize = 14.sp,
+                    fontWeight = FontWeight(400),
+
+                    )
+            }
+
+        }
+    }
 }
--- Aluvery/app/src/main/java/com/example/aluvery/ui/theme/Color.kt
+++ Aluvery/app/src/main/java/com/example/aluvery/ui/theme/Color.kt
@@ -2,6 +2,8 @@ package com.example.aluvery.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
+val Purple500 = Color(0xFF6100EE)
+val Teal200 = Color(0xFF03DAC5)
 val Purple80 = Color(0xFFD0BCFF)
 val PurpleGrey80 = Color(0xFFCCC2DC)
 val Pink80 = Color(0xFFEFB8C8)
